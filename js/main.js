582d31a6a8d18387a5e1ab38d61525af82ab2735
Roman <mamchiy@yandex.ru>
2021-02-28 03:12:16 +0300

    add product_img

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, context: 'img/netbuk_small.jpg'},
+    {id: 2, title: 'Mouse', price: 20, context: 'img/netbuk_small.jpg'},
+    {id: 3, title: 'Keyboard', price: 200, context: 'img/netbuk_small.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, context: 'img/netbuk_small.jpg'},
+    {id: 5, title: 'Gamepad2', price: 10, context: 'img/netbuk_small.jpg'},
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, context) => {
     return `<div class="product-item">
+                <img src="${context}" alt="foto">
                 <h3>${title}</h3>
-                <p>${price}</p>
+                <p>Цена: ${price} руб.</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item.title, item.price, item.context));
     console.log(productsList);
     document.querySelector('.products').innerHTML = productsList;
 };
