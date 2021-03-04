86ffc4535ee04defb73cbd118384b26ff0ec1c0a
Roman <mamchiy@yandex.ru>
2021-03-05 02:12:09 +0300

    trying to implement a bucket via OOP in ES6

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-// const products = [
-//     {id: 1, title: 'Notebook', price: 40000, context: 'img/netbuk.jpg'},
-//     {id: 2, title: 'Mouse', price: 200, context: 'img/mouse.jpg'},
-//     {id: 3, title: 'Keyboard', price: 800, context: 'img/keyboard.jpg'},
-//     {id: 4, title: 'Gamepad', price: 500, context: 'img/gamepad.jpg'},
-//     {id: 5, title: 'TV', price: 100000, context: 'img/TV.jpg'},
-//     {id: 6, title: 'Headphones', price: 500, context: 'img/headphones.jpg'},
-//     {id: 7, title: 'Microphone', price: 500, context: 'img/microphone.jpg'},
-//     {id: 8, title: 'Telephone', price: 500, context: 'img/telephone.jpg'},
-//     {id: 9},
-//     {id: 10},
-//     {id: 11},
-//     {id: 12},
-// ];
-
-// //Функция для формирования верстки каждого товара
-// const renderProduct = (title='No product available', price=0, context='img/not_foto.png') => {
-//     return `<div class="product-item">
-//                 <img class="image" src="${context}" alt="photo products">
-//                 <h3>${title}</h3>
-//                 <p>Цена: ${price} руб.</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// };
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item.title, item.price, item.context)).join('');
-//     console.log(productsList);
-//     document.querySelector('.products').innerHTML = productsList;
-// };
-//
-// renderPage(products);
-
 class ProductsList {
     constructor(container = '.products') {
         this.container = container;
@@ -47,10 +15,10 @@ class ProductsList {
             {id: 6, title: 'Headphones', price: 500, img: 'img/headphones.jpg'},
             {id: 7, title: 'Microphone', price: 500, img: 'img/microphone.jpg'},
             {id: 8, title: 'Telephone', price: 500, img: 'img/telephone.jpg'},
-            {id: 9, title:'No product available', price:0, img:'img/not_foto.png'},
-            {id: 10},
-            {id: 11},
-            {id: 12},
+            {id: 9, title: 'No product available', price: 0, img: 'img/not_foto.png'},
+            {id: 10, title: 'No product available', price: 0, img: 'img/not_foto.png'},
+            {id: 11, title: 'No product available', price: 0, img: 'img/not_foto.png'},
+            {id: 12, title: 'No product available', price: 0, img: 'img/not_foto.png'},
         ];
     }
 
@@ -84,5 +52,16 @@ class ProductItem {
     }
 }
 
+
+class BasketList extends BasketList {
+    constructor(container = '.btn-cart') {
+        super.container = container;
+        super._fetchProducts();
+        this.sum = sum;
+    }
+}
+
 let list = new ProductsList();
+let list2 = new BasketList();
 list.render();
+list2.render();
