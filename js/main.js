ffd359a375f3324dc6f4188eee08ad6b992c8480
Roman <mamchiy@yandex.ru>
2021-03-05 00:08:03 +0300

    add products in goods list

--- js/main.js
+++ js/main.js
@@ -39,15 +39,15 @@ class ProductsList {
 
     _fetchProducts() {
         this.goods = [
-            {id: 1, title: 'Notebook', price: 40000},
-            {id: 2, title: 'Mouse', price: 200},
-            {id: 3, title: 'Keyboard', price: 800},
-            {id: 4, title: 'Gamepad', price: 500},
-            {id: 5, title: 'TV', price: 100000},
-            {id: 6, title: 'Headphones', price: 500},
-            {id: 7, title: 'Microphone', price: 500},
-            {id: 8, title: 'Telephone', price: 500},
-            {id: 9},
+            {id: 1, title: 'Notebook', price: 40000, img: 'img/netbuk.jpg'},
+            {id: 2, title: 'Mouse', price: 200, img: 'img/mouse.jpg'},
+            {id: 3, title: 'Keyboard', price: 800, img: 'img/keyboard.jpg'},
+            {id: 4, title: 'Gamepad', price: 500, img: 'img/gamepad.jpg'},
+            {id: 5, title: 'TV', price: 100000, img: 'img/TV.jpg'},
+            {id: 6, title: 'Headphones', price: 500, img: 'img/headphones.jpg'},
+            {id: 7, title: 'Microphone', price: 500, img: 'img/microphone.jpg'},
+            {id: 8, title: 'Telephone', price: 500, img: 'img/telephone.jpg'},
+            {id: 9, title:'No product available', price:0, img:'img/not_foto.png'},
             {id: 10},
             {id: 11},
             {id: 12},
@@ -67,16 +67,16 @@ class ProductsList {
 
 
 class ProductItem {
-    constructor(product, img = 'https://placehold.it/200x150') {
+    constructor(product, img) {
         this.title = product.title;
         this.price = product.price;
         this.id = product.id;
-        this.img = img;
+        this.img = product.img;
     }
 
     render() {
         return `<div class="product-item" data-id="${this.id}">
-                <img src="${this.img}" alt="Some img">
+                <img class="image" src="${this.img}" alt="Some img">
                 <h3>${this.title}</h3>
                 <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
